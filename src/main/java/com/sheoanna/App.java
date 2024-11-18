ce541434f039e65fd22ca51414cb0d581e86b036
NepyAnna <nepyann@gmail.com>
2024-11-18 09:56:02 +0100

    add class BmiView and BmiController

--- src/main/java/com/sheoanna/App.java
+++ src/main/java/com/sheoanna/App.java
@@ -1,8 +1,6 @@
 package com.sheoanna;
 
-/**
- * Hello world!
- */
+
 public final class App {
     private App() {
     }
@@ -12,6 +10,13 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("                          ");
+        System.out.println("Welcome to BMI calculator!");
+        System.out.println("--------------------------");
+        System.out.println("                          ");
+
+       //Необхідні екземпляри
+
+ //Запускаємо метод, який активує потік програми
     }
 }
